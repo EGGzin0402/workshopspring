795c502fb8a538f54668e869738ff73b666f82c8
Eliel Godoy <eliel.a.m.godoy@gmail.com>
2023-05-12 15:23:49 -0300

    Formulário web feito em Java com Spring

---
+++ src/main/java/br/com/eteczl/workshopspring/AlunoController.java
@@ -0,0 +1,45 @@
+package br.com.eteczl.workshopspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AlunoController {
+
+    private List<Aluno> listagemAlunos = new ArrayList<>();
+    @GetMapping("/alunos")
+    public ModelAndView listagem(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("listagem");
+        modelAndView.addObject("listagemAlunos",listagemAlunos);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastro-alunos")
+    public RedirectView cadastroSalvar(Aluno aluno){
+
+        listagemAlunos.add(aluno);
+
+        return new RedirectView("/alunos");
+
+    }
+
+    @GetMapping("/cadastro-alunos")
+    public ModelAndView cadastroAcesso(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("cadastro");
+
+
+        return modelAndView;
+
+    }
+
+}
---
+++ src/main/java/br/com/eteczl/workshopspring/HomeController.java
@@ -0,0 +1,26 @@
+package br.com.eteczl.workshopspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        Aluno aluno = new Aluno(
+                "Eliel Godoy",
+                "eliel.godoy@etec.sp.gov.br"
+        );
+
+        modelAndView.addObject("primeroAluno", aluno);
+
+        return modelAndView;
+    }
+
+
+}
